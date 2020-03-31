9dcc4d24ba0cccbe2bcdaaf6f1acfa0b6fc4a607
HanFenA7 <tancan137@foxmail.com>
2020-03-31 23:53:16 +0800

    Up 404

--- 404.php
+++ 404.php
@@ -5,23 +5,64 @@
 .indo-404 {
 	text-align:center
 }
-
+.DreamCat-content-header {
+    min-height: 50px;
+    margin-left: -240px;
+    padding: 10px 16px 48px 256px;
+    color: #fff;
+    background-color: #3f51b5;
+    text-shadow: 0 1px 1px rgba(0,0,0,0.2);
+}
 </style>
-	<div class="mdui-color-theme mdui-typo-display-3">
-		<div class="indo-404"
-        <span>404<span class="mdui-404"><?php _e('<br>这个页面不见了?');?></span></span>
+<div class="mdui-container-fluid">
+    <div class="mdui-color-theme mdui-row DreamCat-content-header">
+        <div class="mdui-container fade-scale in">
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<div class="mdui-typo-display-3">
+				<div class="indo-404">
+		        <span>404<span class="mdui-404"><?php _e('<br>这个页面不见了?');?></span></span>
+		        </div>
+			</div>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<div class="mdui-row indo-404">
+				<a href="<?php $this->options->siteUrl(); ?>">
+					<button class="mdui-btn mdui-btn-raised mdui-btn-dense mdui-color-theme-accent mdui-ripple">返回首页</button>
+				</a>
+				<a onclick="javascript :history.back(-1);">
+					<button class="mdui-btn mdui-btn-raised mdui-btn-dense mdui-color-theme-accent mdui-ripple">返回上一页</button>
+				</a>
+			</div>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
         </div>
-	</div>
-
-<div class="mdui-divider"></div>
-<div class="mdui-row mdui-color-white">
-	<a href="<?php $this->options->siteUrl(); ?>"
-		<button class="mdui-btn mdui-btn-raised mdui-btn-dense mdui-color-theme-accent mdui-ripple col-mb-13 mdui-col-md-1 mdui-col-offset-md-3">返回首页</button>
-	</a>
-	<a href="<?php $this->options->siteUrl(); ?>"
-		<button class="mdui-btn mdui-btn-raised mdui-btn-dense mdui-color-theme-accent mdui-ripple col-mb-13 mdui-col-md-1 mdui-col-offset-md-3">返回上一页</button>
-	</a>
+    </div>
 </div>
+    
+
 
 
     </div><!-- end #content-->
