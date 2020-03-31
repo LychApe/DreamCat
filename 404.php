515db74d64d07de51357a5e1ef8df30133610271
Dev-Leo <dengziqi52@gmail.com>
2020-03-31 23:13:00 +0800

    ‘update’

--- 404.php
+++ 404.php
@@ -1,16 +1,21 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <?php $this->need('header.php'); ?>
 
-    <div class="col-mb-12 col-tb-8 col-tb-offset-2">
 
-        <div class="error-page">
-            <h2 class="post-title">404 - <?php _e('页面没找到'); ?></h2>
-            <p><?php _e('你想查看的页面已被转移或删除了, 要不要搜索看看: '); ?></p>
-            <form method="post">
-                <p><input type="text" name="s" class="text" autofocus /></p>
-                <p><button type="submit" class="submit"><?php _e('搜索'); ?></button></p>
-            </form>
-        </div>
+<div class="mdui-color-theme mdui-typo-display-3 mdui-center">
+        <span>404<span class="mdui-404"><?php _e('这个页面不见了?');?></span></span>
+</div>
+
+<div class="mdui-divider"></div>
+<div class="mdui-row mdui-color-white">
+	<a href="<?php $this->options->siteUrl(); ?>"
+		<button class="mdui-btn mdui-btn-raised mdui-btn-dense mdui-color-theme-accent mdui-ripple col-mb-13 mdui-col-md-1 mdui-col-offset-md-3">返回首页</button>
+	</a>
+	<a href="<?php $this->options->siteUrl(); ?>"
+		<button class="mdui-btn mdui-btn-raised mdui-btn-dense mdui-color-theme-accent mdui-ripple col-mb-13 mdui-col-md-1 mdui-col-offset-md-3">返回上一页</button>
+	</a>
+</div>
+
 
     </div><!-- end #content-->
 	<?php $this->need('footer.php'); ?>
