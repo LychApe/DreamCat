4b085fa0cff7bdcb4dd7d0cbd0587d7dc2337ddb
whitebearcn <68509375+whitebearcn@users.noreply.github.com>
2021-01-05 09:21:19 +0800

    更新配置文件
    增加更多自定义选项

--- functions.php
+++ functions.php
@@ -4,18 +4,315 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 define("THEME_URL", rtrim(preg_replace('/^'.preg_quote($options->siteUrl, '/').'/', $options->rootUrl.'/', $options->themeUrl, 1),'/'));
 
 function themeConfig($form) {
-	$logoUrl = new Typecho_Widget_Helper_Form_Element_Text('logoUrl', NULL, NULL, _t('站点 LOGO 地址'), _t('在这里填入一个图片 URL 地址, 以在网站标题前加上一个 LOGO'));
-	$form->addInput($logoUrl);
+	$form->addInput(new Typecho_Widget_Helper_Form_Element_Hidden('logoUrl'));
+	$form->addInput(new Typecho_Widget_Helper_Form_Element_Hidden('Dec'));
+	$form->addInput(new Typecho_Widget_Helper_Form_Element_Hidden('title'));
+	$form->addInput(new Typecho_Widget_Helper_Form_Element_Hidden('keywords'));
+	$form->addInput(new Typecho_Widget_Helper_Form_Element_Hidden('description'));
+	$form->addInput(new Typecho_Widget_Helper_Form_Element_Hidden('Decc'));
+	$form->addInput(new Typecho_Widget_Helper_Form_Element_Hidden('commentszs'));
+	$form->addInput(new Typecho_Widget_Helper_Form_Element_Hidden('headbj1'));
+	$form->addInput(new Typecho_Widget_Helper_Form_Element_Hidden('headbj2'));
+	$form->addInput(new Typecho_Widget_Helper_Form_Element_Hidden('qq'));
+	$form->addInput(new Typecho_Widget_Helper_Form_Element_Hidden('weixin'));
+	$form->addInput(new Typecho_Widget_Helper_Form_Element_Hidden('zfb'));
+	$form->addInput(new Typecho_Widget_Helper_Form_Element_Hidden('weibo'));
+	$form->addInput(new Typecho_Widget_Helper_Form_Element_Hidden('baidusl'));
+	$form->addInput(new Typecho_Widget_Helper_Form_Element_Hidden('articletime'));
+	$options = Typecho_Widget::widget('Widget_Options');
+		$Html = <<<HTML
+	<link rel="stylesheet" href="/usr/themes/echo/layui/css/layui.css?v=2.5.6">
+	<script src="/usr/themes/echo/layui/layui.js?v=2.5.6"></script>
 	
-	/*$sidebarBlock = new Typecho_Widget_Helper_Form_Element_Checkbox('sidebarBlock', 
-	array('ShowRecentPosts' => _t('显示最新文章'),
-	'ShowRecentComments' => _t('显示最近回复'),
-	'ShowCategory' => _t('显示分类'),
-	'ShowArchive' => _t('显示归档'),
-	'ShowOther' => _t('显示其它杂项')),
-	array('ShowRecentPosts', 'ShowRecentComments', 'ShowCategory', 'ShowArchive', 'ShowOther'), _t('侧边栏显示'));
+<div style="background-color: #F2F2F2;">
+  <div class="layui-row layui-col-space15">
+    <div class="layui-col-md12">
+      <div class="layui-card">
+   <br>
+        <div class="layui-card-body">
+	<fieldset class="layui-elem-field">
 	
-	$form->addInput($sidebarBlock->multiMode());*/
+		<legend>DreamCat 主题配置中心 <small> Version:1.7</small></legend>
+
+      	<fieldset class="layui-elem-field layui-field-title" style="margin-top: 30px;">
+		<legend>基本设置</legend>
+		</fieldset>
+		<div class="layui-field-box">
+			<div class="layui-form-item">
+				<label class="layui-form-label">头像图片<br/>地址</label>
+				<div class="layui-input-block">
+					<input type="text" name="dreamcat_logoUrl" value="{$options->logoUrl}" placeholder="网站 LOGO 地址" autocomplete="off" class="layui-input">
+					<div class="layui-word-aux">在这里填入一个图片 URL 地址, 以在网站前台显示头像，留空则显示默认头像</div>
+				</div>
+			</div>
+			<div class="layui-form-item">
+				<label class="layui-form-label">网站标题</label>
+				<div class="layui-input-block">
+					<input type="text" name="dreamcat_title" value="{$options->title}" placeholder="网站标题" autocomplete="off" class="layui-input">
+					<div class="layui-word-aux">留空则不显示</div>
+				</div>
+			</div>
+				<div class="layui-form-item">
+				<label class="layui-form-label">网站<br/>SEO描述</label>
+				<div class="layui-input-block">
+					<input type="text" name="dreamcat_description" value="{$options->description}" placeholder="网站SEO描述" autocomplete="off" class="layui-input">
+					<div class="layui-word-aux">网站描述将显示在网页代码的头部,留空则不显示</div>
+				</div>
+			</div>
+			<div class="layui-form-item">
+				<label class="layui-form-label">网站<br/>SEO关键字</label>
+				<div class="layui-input-block">
+					<input type="text" name="dreamcat_keywords" value="{$options->keywords}" placeholder="网站SEO关键字" autocomplete="off" class="layui-input">
+					<div class="layui-word-aux">请以半角逗号 "," 分割多个关键字</div>
+				</div>
+			</div>
+			<fieldset class="layui-elem-field layui-field-title" style="margin-top: 30px;">
+		<legend>首页设置</legend>
+		</fieldset>
+			<div class="layui-form-item">
+				<label class="layui-form-label">首页标语</label>
+				<div class="layui-input-block">
+					<input type="text" name="dreamcat_Dec" value="{$options->Dec}" placeholder="填入首页标语" autocomplete="off" class="layui-input">
+					<div class="layui-word-aux">填入首页标语</div>
+				</div>
+			</div>
+			<div class="layui-form-item">
+				<label class="layui-form-label">站点首页<br/>博主栏<br/>显示文字</label>
+				<div class="layui-input-block">
+					<input type="text" name="dreamcat_Decc" value="{$options->Decc}" placeholder="填入一个具有逼格的首页博主栏简介或者说明" autocomplete="off" class="layui-input">
+					<div class="layui-word-aux">填入一个具有逼格的首页博主栏简介或者说明</div>
+				</div>
+			</div>
+			<div class="layui-form-item">
+				<label class="layui-form-label">文章可评论<br/>的字数</label>
+				<div class="layui-input-block">
+					<input type="text" name="dreamcat_commentszs" value="{$options->commentszs}" placeholder="填入一个数字" autocomplete="off" class="layui-input">
+					<div class="layui-word-aux">填入一个数字,限制评论的字数</div>
+				</div>
+			</div>
+			<div class="layui-form-item">
+				<label class="layui-form-label">首页公告栏<br/>背景地址</label>
+				<div class="layui-input-block">
+					<input type="text" name="dreamcat_headbj1" value="{$options->headbj1}" placeholder="填入一个图片地址" autocomplete="off" class="layui-input">
+					<div class="layui-word-aux">填入一个图片地址，不填则显示默认背景(地球)</div>
+				</div>
+			</div>
+			<div class="layui-form-item">
+				<label class="layui-form-label">首页博主栏<br/>背景地址</label>
+				<div class="layui-input-block">
+					<input type="text" name="dreamcat_headbj2" value="{$options->headbj2}" placeholder="填入一个图片地址" autocomplete="off" class="layui-input">
+					<div class="layui-word-aux">填入一个图片地址，不填则显示默认背景(地球)</div>
+				</div>
+			</div>
+				<div class="layui-form-item">
+				<div class="layui-inline">
+					<label class="layui-form-label">首页文章<br/>是否显示<br/>发表时间</label>
+					<div class="layui-input-inline" style="width: 80px;">
+						<input type="checkbox" name="dreamcat_articletime" value="checked" lay-skin="switch" lay-text="开启|关闭" {$options->articletime} >
+						<div class="layui-word-aux">开启则首页文章显示发表时间</div>
+					</div>
+					
+				</div>
+			</div>
+			<fieldset class="layui-elem-field layui-field-title" style="margin-top: 30px;">
+		<legend>联系方式设置</legend>
+		</fieldset>
+			<div class="layui-form-item">
+				<label class="layui-form-label">您的QQ<br/>二维码图片<br/>地址</label>
+				<div class="layui-input-block">
+					<input type="text" name="dreamcat_qq" value="{$options->qq}" placeholder="填写QQ二维码图片地址" autocomplete="off" class="layui-input">
+					<div class="layui-word-aux">填写您的QQ二维码图片地址,它将在首页以及其他地方显示，不填则不显示</div>
+				</div>
+			</div>
+			<div class="layui-form-item">
+				<label class="layui-form-label">您的微信<br/>二维码图片<br/>地址</label>
+				<div class="layui-input-block">
+					<input type="text" name="dreamcat_weixin" value="{$options->weixin}" placeholder="填写微信二维码图片地址" autocomplete="off" class="layui-input">
+					<div class="layui-word-aux">填写您的微信二维码图片地址,它将在首页以及其他地方显示，不填则不显示</div>
+				</div>
+			</div>
+			<div class="layui-form-item">
+				<label class="layui-form-label">您的支付宝<br/>二维码图片<br/>地址</label>
+				<div class="layui-input-block">
+					<input type="text" name="dreamcat_zfb" value="{$options->zfb}" placeholder="填写支付宝二维码图片地址" autocomplete="off" class="layui-input">
+					<div class="layui-word-aux">填写您的支付宝二维码图片地址,它将在首页以及其他地方显示，不填则不显示</div>
+				</div>
+			</div>
+			<div class="layui-form-item">
+				<label class="layui-form-label">您的微博<br/>二维码图片<br/>地址</label>
+				<div class="layui-input-block">
+					<input type="text" name="dreamcat_weibo" value="{$options->weibo}" placeholder="填写微博二维码图片地址" autocomplete="off" class="layui-input">
+					<div class="layui-word-aux">填写您的微博二维码图片地址,它将在首页以及其他地方显示，不填则不显示</div>
+				</div>
+			</div>
+			<fieldset class="layui-elem-field layui-field-title" style="margin-top: 30px;">
+		<legend>文章页设置</legend>
+		</fieldset>
+			<div class="layui-form-item">
+				<div class="layui-inline">
+					<label class="layui-form-label">是否开启<br/>百度收录显示</label>
+					<div class="layui-input-inline" style="width: 80px;">
+						<input type="checkbox" name="dreamcat_baidusl" value="checked" lay-skin="switch" lay-text="开启|关闭" {$options->baidusl} >
+						<div class="layui-word-aux">开启则文章内显示百度是否收录</div>
+					</div>
+					
+				</div>
+			</div>
+			<hr>
+			
+			</div>
+			</fieldset>
+	<br><br></div>	</div>	
+
+	
+		
+
+	<script>
+
+		layui.use(["colorpicker","form","transfer"], function(){
+			var $ = layui.$;
+			var form = layui.form;
+			var colorpicker = layui.colorpicker;
+			var transfer = layui.transfer;
+
+			colorpicker.render({
+				elem: '#headerColor'
+				,color: '{$options->headerColor}'
+				,done: function(color){
+					$('#headerColor-input').focus().val(color).blur();
+				}
+			});
+			colorpicker.render({
+				elem: '#headerTextColor'
+				,color: '{$options->headerTextColor}'
+				,done: function(color){
+					$('#headerTextColor-input').focus().val(color).blur();
+				}
+			});
+			colorpicker.render({
+				elem: '#footerColor'
+				,color: '{$options->footerColor}'
+				,done: function(color){
+					$('#footerColor-input').focus().val(color).blur();
+				}
+			});
+			colorpicker.render({
+				elem: '#footerTextColor'
+				,color: '{$options->footerTextColor}'
+				,done: function(color){
+					$('#footerTextColor-input').focus().val(color).blur();
+				}
+			});
+			colorpicker.render({
+				elem: '#backgroundColor'
+				,color: '{$options->backgroundColor}'
+				,done: function(color){
+					$('#backgroundColor-input').focus().val(color).blur();
+				}
+			});
+
+			
+			transfer.render({
+				elem: '#sidebar'
+				,title: ['可选列表', '显示列表']
+				,data: [
+					{"value": "1", "title": "搜索框"}
+					,{"value": "2", "title": "栏目分类"}
+					,{"value": "3", "title": "广而告之"}
+					,{"value": "4", "title": "博主动态"}
+					,{"value": "5", "title": "标签云"}
+					,{"value": "6", "title": "友情链接"}
+				]
+				,value: [{$options->sidebar}]
+				,id: 'sidebar'
+				,onchange: function(data, index){
+					var sidebar = [];
+					var sidebarData = transfer.getData('sidebar');
+					for(var key in sidebarData){
+						sidebar.push(sidebarData[key]['value']);
+
+					}
+					$("input[name='sidebar']").val(sidebar.join());
+				}
+			});
+
+			$("form").addClass("layui-form");
+			$("input[name=echo_thumbType][value='']").attr("checked", '{$options->thumbType}' == '' ? true : false);
+			$("input[name=echo_thumbType][value='1']").attr("checked", '{$options->thumbType}' == 1 ? true : false);
+			$("input[name=echo_thumbType][value='2']").attr("checked", '{$options->thumbType}' == 2 ? true : false);
+			$("input[name=echo_thumbType][value='3']").attr("checked", '{$options->thumbType}' == 3 ? true : false);
+			form.render();
+
+			//同步input值
+			$('input').bind('input propertychange blur', function(){
+				var name = $(this).attr("name").split('_')[1];
+				$("input[name='"+name+"']").val($(this).val());
+			});
+			$('textarea').bind('input propertychange', function(){
+				var name = $(this).attr("name").split('_')[1];
+				$("input[name='"+name+"']").val($(this).val());
+			});
+			form.on('switch()', function(data){
+				var that = data.elem;
+				var name = $(that).attr("name").split('_')[1];
+				var value = data.elem.checked?data.value:'';
+				$("input[name='"+name+"']").val(value);
+			}); 
+			form.on('radio()', function(data){
+				var that = data.elem;
+				var name = $(that).attr("name").split('_')[1];
+				$("input[name='"+name+"']").val(data.value);
+			});
+		});
+	</script>
+	<script>
+	layui.use('element', function(){
+  var $ = layui.jquery
+  ,element = layui.element; //Tab的切换功能，切换事件监听等，需要依赖element模块
+  
+  //触发事件
+  var active = {
+    tabAdd: function(){
+      //新增一个Tab项
+      element.tabAdd('demo', {
+        title: '新选项'+ (Math.random()*1000|0) //用于演示
+        ,content: '内容'+ (Math.random()*1000|0)
+        ,id: new Date().getTime() //实际使用一般是规定好的id，这里以时间戳模拟下
+      })
+    }
+    ,tabDelete: function(othis){
+      //删除指定Tab项
+      element.tabDelete('demo', '44'); //删除：“商品管理”
+      
+      
+      othis.addClass('layui-btn-disabled');
+    }
+    ,tabChange: function(){
+      //切换到指定Tab项
+      element.tabChange('demo', '22'); //切换到：用户管理
+    }
+  };
+  
+  $('.site-demo-active').on('click', function(){
+    var othis = $(this), type = othis.data('type');
+    active[type] ? active[type].call(this, othis) : '';
+  });
+  
+  //Hash地址的定位
+  var layid = location.hash.replace(/^#test=/, '');
+  element.tabChange('test', layid);
+  
+  element.on('tab(test)', function(elem){
+    location.hash = 'test='+ $(this).attr('lay-id');
+  });
+  
+});
+
+</script>
+HTML;
+	$layout = new Typecho_Widget_Helper_Layout();
+	$layout->html(_t($Html));
+	$form->addItem($layout);
 }
 
 /* 随机图片 */
@@ -95,10 +392,36 @@ $link = '<a href="' . $content['permalink'] . '" class="mdui-ripple mdui-col-xs-
 	  </a>';
 echo $link;
 } else {
+
 echo $default;
 }
 }
 
+//判断内容页是否百度收录
+
+function baidu_record() {
+$url='http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; 
+
+if(checkBaidu($url)==1)
+{echo "百度已收录";
+}
+else
+{echo "<a style=\"color:red;\" rel=\"external nofollow\" title=\"点击提交收录！\" target=\"_blank\" href=\"https://ziyuan.baidu.com/sitesubmit/index?sitename=$url\">百度未收录</a>";}
+}
+
+   function checkBaidu($url) { 
+    $url = 'https://www.baidu.com/s?wd=' . urlencode($url); 
+    $curl = curl_init(); 
+    curl_setopt($curl, CURLOPT_URL, $url); 
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); 
+    $rs = curl_exec($curl); 
+    curl_close($curl); 
+    if (!strpos($rs, '没有找到')) { //没有找到说明已被百度收录 
+        return 1; 
+    } else { 
+        return -1; 
+    } 
+}
 
 function art_count ($cid){
 $db=Typecho_Db::get ();
