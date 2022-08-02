69010a0763fc9db0956aee140579b060118abbdd
HanFengA7 <tancan137@foxmail.com>
2022-08-02 08:44:47 +0800

    [style]:code

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    printWidth: 100,
+    tabWidth: 2, // 超过最大值换行
+    semi: false, // 结尾不用分号
+    singleQuote: true, // 使用单引号
+    disableLanguages: ["vue"], // 不格式化vue文件，vue文件的格式化单独设置
+    htmlWhitespaceSensitivity: "ignore",
+  }
--- 404.php
+++ 404.php
@@ -1,5 +1,5 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
-<?php $this->need('header.php'); ?>
+<?php $this->need('header.php'); ?> 
 <?php $this->need('tpl/PageHearder.php'); ?>
 		<div class="mdui-container fade-scale in">
 			<div class="mdui-typo-display-3">
