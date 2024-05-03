d26a9de3bb78514005c90f1bb51c7d7bfccb159a
CornWorld <cw@had.ink>
2024-05-03 22:34:54 +0800

    [Feat] Use more concise titles

--- header.php
+++ header.php
@@ -20,12 +20,15 @@
     <meta charset="<?php $this->options->charset(); ?>">
     <meta name="renderer" content="webkit">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <title><?php $this->archiveTitle([
-            'category' => _t('分类 %s 下的文章'),
-            'search' => _t('包含关键字 %s 的文章'),
-            'tag' => _t('标签 %s 下的文章'),
-            'author' => _t('%s 发布的文章')
-        ], '', ' - '); ?><?php $this->options->DC_WebName(); ?></title>
+	<title >
+        <?php $this->archiveTitle([
+            'category' => _t('分类：%s'),
+            'search' => _t('搜索结果：%s'),
+            'tag' => _t('标签：%s'),
+            'author' => _t('作者：%s'),
+            'date' => _t('归档：%s')
+        ], '', ' - ') . $this->options->DC_WebName(); ?>
+	</title >
     <?php
     if ($this->options->DC_WebCdnRadio == 'FuseAccelerationMode') {
         echo '<link rel="dns-prefetch" href="//gh.sourcegcdn.com">';
